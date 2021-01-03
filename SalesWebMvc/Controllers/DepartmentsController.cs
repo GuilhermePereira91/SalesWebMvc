0aeb2a177cc0a12b6c02115f8b8f3e3704ff6333
GuilhermePereira91 <guilhermexmarques91@gmail.com>
2021-01-02 22:30:25 -0300

    Primeiro teste MVC: Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departments> list = new List<Departments>();
+            list.Add(new Departments { id = 1, name = "Eletronicos" });
+            list.Add(new Departments { id = 2, name = "Fashion" });
+
+
+            return View(list);
+        }
+    }
+}
